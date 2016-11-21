224d2bffb610729295f17f980df7aea315eed9ba
Luis Enrique Tierrafria Rodriguez <luis@Luiss-iMac.lan>
2016-11-21 01:21:26 -0600

    end

--- calculadora/ViewController.swift
+++ calculadora/ViewController.swift
@@ -10,11 +10,81 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelResult: UILabel!
+    
+    var result:Float = 0
+    
+    var actualNum:Float = 0
+    
+    var actualOp:String = "="
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        labelResult.text = "\(result)"
+        
     }
 
+    @IBAction func num(sender: UIButton) {
+        
+        actualNum = actualNum * 10 + Float(Int(sender.titleLabel!.text!)!)
+        
+        labelResult.text = "\(actualNum)"
+    }
+    
+    
+    @IBAction func op(sender: UIButton) {
+        
+        switch actualOp {
+        case "=":
+            result = actualNum
+        case "+":
+            result = result + actualNum
+        case "-":
+            result = result - actualNum
+        case "*":
+            result = result * actualNum
+        case "/":
+            result = result / actualNum
+        default:
+            print("error")
+        }
+        
+        actualNum = 0
+        labelResult.text = ("\(result)")
+        
+        if (sender.titleLabel!.text == "=") {
+            result = 0
+        }
+        
+        actualOp = sender.titleLabel!.text! as String
+    }
+    
+    
+    @IBAction func clear(sender: UIButton) {
+        
+        result = 0
+        actualNum = 0
+        actualOp = "="
+        labelResult.text = ("\(result)")
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
